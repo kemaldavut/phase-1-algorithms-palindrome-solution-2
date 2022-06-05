3fe4227301a2a7c48f0bed3252a1208f225cdc2f
Kemal DAVUT <mustafakemaldavut@gmail.com>
2022-06-05 23:57:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function isPalindrome(word) {
+  let le = word.length;
+  if ( le === 0  || le === 1) {
+    return true;
+  }
+  if (word[0] === word[le-1]) {
+    return isPalindrome(word.slice(1, le-1));
+  }
+  return false;
   // Write your algorithm here
 }
 
@@ -8,6 +16,11 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  1- function isPalindrome(word)
+  2-save the length of the word
+  3-if the length of the word is 0 or 1 return true
+  4-if the first and last letter are equal return isPalindrome(word.slice(1, le-1))
+  5-if the first and last letter are not equal return false
 */
 
 // You can run `node index.js` to view these console logs
